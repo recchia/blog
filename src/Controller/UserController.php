416edf1008b2821f143a49bb05fa7ff2249eddf4
recchia <piero.recchia@gmail.com>
2018-06-03 20:12:11 -0300

    Added logout link and fix register controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,7 @@ use App\Security\LoginFormAuthenticator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class UserController extends Controller
@@ -17,12 +18,14 @@ class UserController extends Controller
      * @param Request $request
      * @param GuardAuthenticatorHandler $authenticatorHandler
      * @param LoginFormAuthenticator $authenticator
+     * @param UserPasswordEncoderInterface $encoder
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
     public function register(
         Request $request,
         GuardAuthenticatorHandler $authenticatorHandler,
-        LoginFormAuthenticator $authenticator
+        LoginFormAuthenticator $authenticator,
+        UserPasswordEncoderInterface $encoder
     )
     {
         $form = $this->createForm(UserRegistrationFormType::class);
@@ -34,7 +37,7 @@ class UserController extends Controller
             if ($form->isValid()) {
                 /** @var User $user */
                 $user = $form->getData();
-                $user->setPassword($user->getPlainPassword());
+                $user->setPassword($encoder->encodePassword($user, $user->getPlainPassword()));
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($user);
                 $em->flush();
